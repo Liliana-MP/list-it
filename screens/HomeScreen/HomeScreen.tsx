d421b59c5c519c274baf74d61607dc708746a4fe
Liliana Montini Pitra <liliana.pitra@gmail.com>
2021-12-15 12:31:47 +0100

    removed header and added cog icon to homescreen

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { View, Text } from "react-native";
-import Header from "../../components/Header";
+import { Text, TouchableOpacity } from "react-native";
+import { CogIcon } from "react-native-heroicons/outline";
 import { ScreenRoute } from "../../navigation/constants";
 import { RootStackParamList } from "../../navigation/types";
 import * as S from "./styled";
@@ -14,11 +14,11 @@ type HomeScreenProps = NativeStackScreenProps<
 const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
     <S.Container>
-      <Header
-        handleSettingsOnPress={() =>
-          navigation.navigate(ScreenRoute.LIST_SETTINGS_SCREEN)
-        }
-      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate(ScreenRoute.LIST_SETTINGS_SCREEN)}
+      >
+        <CogIcon height={30} width={30} color="white" />
+      </TouchableOpacity>
       <Text>This is HomeScreen</Text>
     </S.Container>
   );
